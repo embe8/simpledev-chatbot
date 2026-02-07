d0ac871a197b7e5785818e056cc2a06f071686a9
embe8 <eebon@icloud.com>
2026-02-06 20:14:52 -0800

    Move header.css to pages and starting-code/general to index.css

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
-import { Header } from '../components/Header';
-import './HomePage.css';
 
 export function HomePage() {
   return (
     <>
       <title>Ecommerce Project</title>
 
-      <Header />
+     
 
       <div className="home-page">
         <div className="products-grid">
