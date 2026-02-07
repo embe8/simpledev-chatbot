15bc5691ab2c676219ce33f3767eb587538d75fa
embe8 <eebon@icloud.com>
2026-02-06 21:17:21 -0800

    Add Routes to main.jsx

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage';
 import './App.css'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="checkout" element={<div>test checkout</div>}></Route>
+    </Routes>
+  
+
   )
 }
 
