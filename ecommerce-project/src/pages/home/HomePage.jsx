5475ee9ccb96b0e43eb147c5b17b66a8673cbaa4
embe8 <eebon@icloud.com>
2026-02-11 22:15:26 -0800

    Modify HomePage to use async function

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -7,16 +7,13 @@ import './HomePage.css';
 export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
-
-
   useEffect(() => {
-    axios.get('/api/products')
-    .then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get('/api/products');
       setProducts(response.data);
-    });
+    };
 
-    
-
+    getHomeData();
   }, []);//dependency array
  
 
